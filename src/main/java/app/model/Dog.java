125de9f36b513a26e550f1962298e68f481a8d23
Anton Knyazev <bearider@mail.ru>
2022-04-05 15:43:42 +0300

    Exercise solved

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("Dog")
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "I'm a dog";
+    }
+}
