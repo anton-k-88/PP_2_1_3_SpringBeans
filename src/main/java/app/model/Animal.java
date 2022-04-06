433c6b104f7d7fb17e5f5cbac3dd8ac98689b58b
Anton Knyazev <bearider@mail.ru>
2022-04-06 14:48:48 +0300

    updated Qualifier

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -4,6 +4,5 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("Animal")
 public abstract class Animal {
 }
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier
 public class Dog extends Animal {
 
     @Override
